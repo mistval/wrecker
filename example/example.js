29f5ecfc7ba2fcd8abedd03a6c6c6bc305cfcf9c
Randall Schmidt <randallschmidt1@gmail.com>
2019-07-28 14:03:28 +0700

    add example

---
+++ example/example.js
@@ -0,0 +1,19 @@
+const WorkerPool = require('../index.js');
+const path = require('path');
+
+const JOBS_MODULE_PATH = path.join(__dirname, 'jobs.js');
+
+const pool = new WorkerPool(JOBS_MODULE_PATH);
+
+async function start() {
+  const a = await pool.doWork('five');
+  const b = await pool.doWork('nine');
+  const sum = await pool.doWork('add', { a, b });
+
+  // Shut down the pool and terminate all worker threads.
+  await pool.shutdown();
+
+  return sum;
+}
+
+start().then(sum => console.log(sum));
---
+++ example/jobs.js
@@ -0,0 +1,11 @@
+module.exports = {
+  five: function() {
+    return 5;
+  },
+  nine: function() {
+    return 9;
+  },
+  add: function(args) {
+    return args.a + args.b;
+  },
+};
